dacb55d22c9981e5c7e8e97740a5dd53540ae0af
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-04-02 22:43:48 +0200

    client: Render actual tweets

--- client/main.js
+++ client/main.js
@@ -1,15 +1,21 @@
 const tweetTemplate = document.querySelector("template#tweet");
 const streamEl = document.querySelector("#stream");
 
-function addTweet(message) {
+function addTweet(status) {
   const tweetEl = document.importNode(tweetTemplate.content, true);
-  tweetEl.querySelector(".message").innerText = message;
+  tweetEl.querySelector(".profile-img").src =
+    status.user.profile_image_url_https;
+  tweetEl.querySelector(".name").innerText = status.user.name;
+  tweetEl.querySelector(".screen-name").innerText = `@${
+    status.user.screen_name
+  }`;
+  tweetEl.querySelector(".created-at").innerText = status.created_at;
+  tweetEl.querySelector(".text").innerText = status.text;
   streamEl.prepend(tweetEl);
 }
 
 const ws = new WebSocket("ws://localhost:8000/ws");
 ws.onmessage = function(event) {
-  addTweet(event.data);
+  const status = JSON.parse(event.data);
+  addTweet(status);
 };
-
-addTweet("Foo");
